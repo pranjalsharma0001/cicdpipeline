4f174dff276bdc7695c65361308e075b612a2484
Pranjal <pranjalsharmakarnal@gmail.com>
2025-05-02 17:06:21 +0530

    second  commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>My React app</h1>
+      <h2>My react implemtation with CI/CD</h2>
+      <h3>Pranjal</h3>
+    </div>
+  )
+}
+
+export default App
